e8a7be09a1d3898419cec75687ded92a376f1f11
Kenji Takayasu <takayasu@kcc-net.co.jp>
2014-09-06 07:52:24 +0900

    rubyで0から10万までを表示する時間を計測

--- zero_one.rb
+++ zero_one.rb
@@ -1,13 +1,17 @@
 # $list = [ 0, 1 ]
-$t = Time.new
-$start_sec = $t.to_i
-$start_micro = $t.usec
-#p $t.to_i
-for i in 0..1000000
+$t = Time.now
+#puts self - $t.time
+# $start_sec = $t.to_i
+# $start_micro = $t.usec
+# p $t.to_i
+for i in 0..100000
   print rand( 2 )
 end
 print "\n"
-$e = Time.new
-$end_sec = $e.to_i
-$end_micro = $e.usec
-p "exec_time is " + ( $end_sec - $start_sec ).to_s + "." + ($end_micro - $start_micro ).to_s
+# $e = Time.new
+# $end_sec = $e.to_i
+# $end_micro = $e.usec
+# p "exec_time is " + ( $end_sec - $start_sec ).to_s + "." + ($end_micro - $start_micro ).to_s
+$e = Time.now
+puts $e - $t
+
